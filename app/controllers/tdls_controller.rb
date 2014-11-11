442aebb1fbe91c652689a57e8156f50ff4f35783
Sirius Wang <larrywhy.tw@gmail.com>
2014-11-11 15:45:13 +0800

    finished creat action and bootstrap layout setting

--- app/controllers/tdls_controller.rb
+++ app/controllers/tdls_controller.rb
@@ -4,6 +4,23 @@ class TdlsController < ApplicationController
     @tdls = Tdl.all
   end
 
+  def new
+    @tdl = Tdl.new
+  end
 
+  def create
+    @tdl = Tdl.new(tdl_params)
+    if @tdl.save
+       redirect_to(tdls_path)
+    else
+       render :new
+    end
+  end
+
+private
+
+  def tdl_params
+    params.require(:tdl).permit(:todo,:isdone)
+  end
 
 end
